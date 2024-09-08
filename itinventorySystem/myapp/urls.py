7ec6acaae25c0b7b54955a338d4969ed026193b6
Kaleb Jones <truth0669@gmail.com>
2024-09-08 17:44:28 -0500

    UPDATED 9/8/2024

--- itinventorySystem/myapp/urls.py
+++ itinventorySystem/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("struct.html", views.struct, name="struct"),
+    path("base.html", views.base, name="base"),
 ]
--- itinventorySystem/myapp/views.py
+++ itinventorySystem/myapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "base.html")
+
+def struct(request):
+    return render(request, "struct.html")
+
+def base(request):
+    return render(request, "base.html")
